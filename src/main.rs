68a1d3539d73b440b7c7a53d42bd933e94a9a102
hov1417 <hovhannes1417@gmail.com>
2025-02-01 18:05:36 +0400

    chore: versions upgrade, formatting

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ async fn axum(#[shuttle_runtime::Secrets] secrets: SecretStore) -> shuttle_axum:
     logging::init_logging();
     let application = ApplicationState::new(Arc::new(secrets));
     let router = Router::new()
-        .route("/feed/:subreddit", get(subreddit_rss))
+        .route("/feed/{subreddit}", get(subreddit_rss))
         .with_state(application);
 
     Ok(router.into())
